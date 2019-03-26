0e564b331e5811b4ee750f0a2a44af66edbf158f
Josef Starychfojtu <josef.starychfojtu@gmail.com>
2019-03-26 17:33:45 +0100

    Navigation drawer

--- SmartRecipes/SmartRecipes.fs
+++ SmartRecipes/SmartRecipes.fs
@@ -2,6 +2,7 @@
 
 open Fabulous.Core
 open Xamarin.Forms
+open Fabulous.DynamicViews
 
 module App =
 
@@ -18,6 +19,7 @@ module App =
     type Message = 
         | LoginPageMessage of LoginPage.Message
         | ShoppingListPageMessage of ShoppingListPage.Message
+        | ChangePage of Page
 
     let initModel = {
         CurrentPage = LoginPage
@@ -29,6 +31,7 @@ module App =
     
     let update msg model =
         match msg with
+        | ChangePage page -> (model, Cmd.none)
         | LoginPageMessage msg ->
              let result = LoginPage.update msg model.LoginPage
              match result with
@@ -38,12 +41,39 @@ module App =
         | ShoppingListPageMessage msg ->
              let (newModel, cmd) = ShoppingListPage.update model.ShoppingListPage msg
              { model with ShoppingListPage = newModel }, Cmd.map (ShoppingListPageMessage) cmd
-         
+             
+    let navigationPages = [| (ShoppingListPage, "ShoppingList") |]
+    let navigationItem (_, text) =
+        View.Label text
+        
+    let first (a, _) = a
+    
+    let shoppingListTabPage shoppingListPage =     
+        dependsOn () (fun _ () -> View.TabbedPage(
+            title = "Shopping list",
+            children = [ shoppingListPage ]
+        ))
+             
+    let appContainer dispatch detail =
+        dependsOn () (fun _ () -> View.MasterDetailPage(
+            detail = detail,
+            master = View.ContentPage(
+                title = "Smart Recipes",
+                content = View.ListView(
+                    items = Seq.map navigationItem navigationPages,
+                    itemTapped = (fun index -> Array.get navigationPages index |> first |> ChangePage |> dispatch)
+                )
+            )
+        ))
 
     let view model dispatch =    
         match model.CurrentPage with
-        | LoginPage -> LoginPage.view model.LoginPage (LoginPageMessage >> dispatch)
-        | ShoppingListPage -> ShoppingListPage.view model.ShoppingListPage (ShoppingListPageMessage >> dispatch)
+        | LoginPage -> 
+            LoginPage.view model.LoginPage (LoginPageMessage >> dispatch)
+        | ShoppingListPage -> 
+            ShoppingListPage.view model.ShoppingListPage (ShoppingListPageMessage >> dispatch) 
+            |> shoppingListTabPage 
+            |> (appContainer dispatch)
 
     let program = Program.mkProgram init update view
 
